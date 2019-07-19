0ed9e6db773f045492c29d3ad39ca38a621df5f2
shabo1984 <shamilastar@gmail.com>
2019-07-19 14:11:02 -0500

    Update setup-gcc.sh
    remove gcc older versions

--- setup-gcc.sh
+++ setup-gcc.sh
@@ -7,10 +7,8 @@ sudo apt update
 sudo update-alternatives --remove-all gcc
 sudo update-alternatives --remove-all g++
 
-sudo apt-get install -y gcc-4.8 g++-4.8 gcc-4.9 g++-4.9 gcc-5 g++-5 gcc-6 g++-6 gcc-7 g++-7
+sudo apt-get install -y gcc-5 g++-5 gcc-6 g++-6 gcc-7 g++-7
 
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 30 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 20 --slave /usr/bin/g++ g++ /usr/bin/g++-6
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 10 --slave /usr/bin/g++ g++ /usr/bin/g++-7
